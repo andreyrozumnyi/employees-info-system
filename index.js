4a600340df6e4f10dfa33aee047fcb69e534dd7f
Andrii Rozumnyi <andrii.rozumnyi@pipedrive.com>
2018-06-06 08:51:14 +0300

    add initial vacation calculation logic

---
+++ index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const signale = require('signale');
+
+const vacation = require('./lib/vacation.js');
+
+let started = false;
+
+program
+	.version('0.0.1', '-v, --version')
+	.description('Employees information system');
+
+program
+	.command('vacation <year> <input-file> [output-file]')
+	.alias('vac')
+	.description('Calculate vacation days for a given year')
+	.action(async (...args) => {
+		started = true;
+		await vacation.calculate(...args);
+	});
+
+program.parse(process.argv);
+
+if (!started) {
+	signale.warn('Cannot find a command. Please see help for it...');
+	program.help();
+}
